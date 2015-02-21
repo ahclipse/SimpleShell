be8e6bbc7af6b587a83da24e4a492199f12debe8
Benjamin Richardson <brichard@adelie-05.cs.wisc.edu>
2015-02-20 21:09:18 -0600

    Broken redirect complete r Please enter the commit message for your changes. Lines starting

--- mysh.c
+++ mysh.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int mysh(void){
 
@@ -16,6 +18,8 @@ int mysh(void){
   char str[maxLength];
   char str_dup[maxLength];
   char **argv;
+  char ** sub_argv;
+  int out;
   int argc;
   int pid;
   int status;//TODO error checking
@@ -70,7 +74,8 @@ int mysh(void){
     exit(0);
   }
   //Code to print out current directory when pwd is inserted
-  if( strcmp( argv[0], pwdString) == 0){ 
+  else if( strcmp( argv[0], pwdString) == 0)
+  { 
     char currentDir[maxLength]; 
     if(getcwd(currentDir, sizeof(currentDir))!= NULL){
       printf("%s\n", currentDir); 
@@ -81,7 +86,7 @@ int mysh(void){
     }
   }
   //check for cd
-  if( strcmp( argv[0], cdString) == 0)
+  else if( strcmp( argv[0], cdString) == 0)
   {
     if( argc == 1 )
     {
@@ -91,7 +96,7 @@ int mysh(void){
     {
       path = argv[1];
     }
-
+    printf( "%s\n", path);
     if( path == NULL ||  chdir(path ) == -1)
     {
       free( argv );
@@ -99,47 +104,68 @@ int mysh(void){
     }
     //TODO exec events after directory change
   }
-
-  //begin exec function
-  if( strchr( str, '>') != NULL )
-  {
-    i = 0;
-    redirectNotFound = 1;
-    while( i <= argc && redirectNotFound )  
-    {
-      //what about args past the first >?
-      if( strcmp( argv[i] ,">>" ) == 0 || strcmp( argv[i] ,">" ) == 0 )
-      {
-        redirectNotFound = 0;//found redirect, assuming only one
-        if(  strcmp( argv[i] ,">>" ) == 0 )
-        {
-          
-        }
-        if(  strcmp( argv[i] ,">" ) == 0 )
-        {
-          
-        }
-        //check i+1, check i +2
-        //change std out
-        //create sub array of args
-      } 
-    }    
-  }
-
-  if( (pid = fork()) == -1)
+  else if( (pid = fork()) == -1)
   {
     return -1;
   }
   else
   {
     if( pid == 0)
-    {
-      if( execvp( argv[0], argv) == -1)
+    {       
+      //begin exec function
+      if( strchr( str, '>') != NULL )
       {
-        //change stdout or std in depending
-        free( argv);
-        return -1;
+        i = 0;
+        redirectNotFound = 1;
+        while( i <= argc && redirectNotFound )  
+        {
+          if( strcmp( argv[i] ,">>" ) == 0 || strcmp( argv[i] ,">" ) == 0 )
+          {
+            redirectNotFound = 0;//found redirect, assuming only one
+        
+            if( argc != i+1)
+            {
+              return -1;// input after filename, or no filename given
+            }
+
+            //change stdout to newfile
+            if(  strcmp( argv[i] ,">>" ) == 0 )
+            {
+              out = open( argv[i], O_APPEND | O_CREAT | O_WRONLY);
+            }
+            if(  strcmp( argv[i] ,">" ) == 0 )
+            {
+              out = open( argv[i], O_TRUNC | O_CREAT | O_WRONLY);
+            }
+
+            if ( out == -1)
+            {
+              return -1;
+            }
+
+            if( dup2( 1, out ) == -1)//1 means stdout????
+            {
+              return -1;
+            }
+
+            //create sub array of args
+            if( (sub_argv = malloc( (argc - 2) * sizeof(char*))) != NULL)
+            {
+              return -1;
+            }
+            memcpy( sub_argv, argv, (argc - 2) * sizeof(char*) );
+            free(argv);
+            argv = sub_argv;//to keep naming in rest of the code the same
+          } 
+        }    
       }
+      if( execvp( argv[0], argv) == -1)
+      { 
+        fprintf(stderr, "Error!\n" );
+        free( argv);
+        exit(0);//child process should close even if exec fails
+      }
+      exit(0);//end child process
     }
     else
     {
