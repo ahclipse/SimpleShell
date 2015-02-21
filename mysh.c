544ca93a3678949d49a8ba94526a045b76bf119b
Benjamin Richardson <brichard@galapagos-07.cs.wisc.edu>
2015-02-21 17:33:21 -0600

    restructure execute

--- mysh.c
+++ mysh.c
@@ -27,7 +27,6 @@ int sub_array(int totalElm, int startIndex, int endIndex, char*** array)
     return -1;
   }
   memcpy( sub_arr, ((char *)(*array + startIndex )), copyNum * sizeof(char*) );
-  printf( "%s", sub_arr[0]);
   sub_arr[copyNum + 1] = NULL;
   free(*array);
   *array = sub_arr;
@@ -41,78 +40,58 @@ int execute( int argc, char **argv)
   int out;
   int i;
   int status;
-
-  if( (pid = fork()) == -1)
+  //begin exec function
+  i = 0;
+  redirectNotFound = 1;
+  while( i < argc && redirectNotFound )  
   {
-    return -1;
+	  if( strcmp( argv[i] ,">>\0" ) == 0 || strcmp( argv[i] ,">\0" ) == 0 )
+	  {
+		  redirectNotFound = 0;//found redirect, assuming only one
+
+		  if( argc < 3 || argc != i+1+1)//one for zero indexing, one for next element to exist
+		  {
+			  return -1;// input after filename, or no filename given
+		  }
+
+		  //change stdout to newfile
+		  if(  strcmp( argv[i] ,">>" ) == 0 )
+		  {
+			  printf(">>");
+			  out = open( argv[i+1], O_APPEND | O_CREAT | O_WRONLY, 0666); 
+		  }
+		  if(  strcmp( argv[i] ,">" ) == 0 )
+		  {
+			  out = open( argv[i+1], O_TRUNC | O_CREAT | O_WRONLY, 0666); 
+		  }
+
+		  if ( out == -1)
+		  {
+			  return -1;
+		  }
+
+		  if( dup2( out, 1 ) ==  -1)//1 means stdout
+		  {
+			  return -1;
+		  }
+
+		  close(out);
+		  //create sub array of args
+		  if( (argc = sub_array( argc, 0 , i, &argv)) == - 1) //to keep naming in rest of the code the same
+		  {
+			  fprintf(stderr, "Error!\n" );
+			  exit(1);//child process should close even if exec fails
+		  }
+     }
+    i++;       
   }
-  else
-  {
-    if( pid == 0)
-    {       
-      //begin exec function
-      i = 0;
-      redirectNotFound = 1;
-      while( i < argc && redirectNotFound )  
-      {
-        if( strcmp( argv[i] ,">>\0" ) == 0 || strcmp( argv[i] ,">\0" ) == 0 )
-        {
-          redirectNotFound = 0;//found redirect, assuming only one
-        
-          if( argc < 3 || argc != i+1+1)//one for zero indexing, one for next element to exist
-          {
-            return -1;// input after filename, or no filename given
-          }
 
-          //change stdout to newfile
-          if(  strcmp( argv[i] ,">>" ) == 0 )
-          {
-	    printf(">>");
-            out = open( argv[i+1], O_APPEND | O_CREAT | O_WRONLY, 0666); 
-          }
-          if(  strcmp( argv[i] ,">" ) == 0 )
-          {
-            out = open( argv[i+1], O_TRUNC | O_CREAT | O_WRONLY, 0666); 
-          }
-
-          if ( out == -1)
-          {
-            return -1;
-          }
-
-          if( dup2( out, 1 ) ==  -1)//1 means stdout
-          {
-            return -1;
-          }
-
-          close(out);
-          //create sub array of args
-          if( (argc = sub_array( argc, 0 , i, &argv)) == - 1) //to keep naming in rest of the code the same
-          {
-             fprintf(stderr, "Error!\n" );
-             exit(1);//child process should close even if exec fails
-          }
-        }
-        i++;       
-      }
-
-      if( execvp( argv[0], argv) == -1)
-      { 
-        fprintf(stderr, "Error!\n" );
-        exit(0);//child process should close even if exec fails
-      }
-      exit(0);//Should never reach here
-    }
-    else
-    {
-      if( waitpid( pid, &status, 0) == -1)
-      {
-        free( argv );
-        return -1;
-      }
-    }
-    return 0;
+  if( execvp( argv[0], argv) == -1)
+  { 
+	  fprintf(stderr, "Error!\n" );
+	  exit(0);//child process should close even if exec fails
   }
+  exit(0);//Should never reach here
 }
 
 int mysh(void)
@@ -128,6 +107,8 @@ int mysh(void)
   char **argv;;
   char currentDir[maxLength]; 
   int argc;
+  int pid;
+  int status;
   int i; //couter for loops;
 
   printf( "mysh>  ");
@@ -174,10 +155,11 @@ int mysh(void)
   { 
     exit(0);
   }
-  else if( strcmp( argv[0], pwdString) == 0)
+  if( strcmp( argv[0], pwdString) == 0)
   { //Code to print out current directory when pwd is inserted
    if(getcwd(currentDir, sizeof(currentDir))!= NULL){
     printf("%s\n", currentDir); 
+    return 0;
     }
     else
     {
@@ -185,7 +167,7 @@ int mysh(void)
     }
   }
   //check for cd
-  else if( strcmp( argv[0], cdString) == 0)
+  if( strcmp( argv[0], cdString) == 0)
   {
     if( argc == 1 )
     {
@@ -201,24 +183,39 @@ int mysh(void)
       free( argv );
       return -1;
     }
-    if( argc > 2)
-    { 
-      if( -1 == (argc = sub_array( argc, 2 , argc , &argv)))
-      {
-        return -1;
-      }
-      if( execute(argc , argv) == -1)
-      {
-        return -1;
-      }
+    if( argc < 3)
+    {
+      return 0; 
+    }
+  
+    if( -1 == (argc = sub_array( argc, 2 , argc , &argv)))
+    {
+      return -1;
     }
   }
-  else if( execute( argc, argv) == -1)
+
+  if( (pid = fork()) == -1)
   {
     return -1;
   }
   else
   {
+    if( pid == 0)
+    {       
+      if( execute( argc, argv) == -1)
+      {
+        return -1;
+       }
+
+     }
+     else
+     {
+        if( waitpid( pid, &status, 0) == -1)
+        {
+          free( argv );
+          return -1;
+        }
+     } 
  //free memory of last input
     free( argv);
   }
