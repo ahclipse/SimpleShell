994fe5cc6c0b9fc82d72660b4633b13159f2c1a8
Benjamin Richardson <brichard@adelie-06.cs.wisc.edu>
2015-02-20 23:46:04 -0600

    Slightly less broken redirect

--- mysh.c
+++ mysh.c
@@ -19,6 +19,7 @@ int mysh(void){
   char str_dup[maxLength];
   char **argv;
   char ** sub_argv;
+  char currentDir[maxLength]; 
   int out;
   int argc;
   int pid;
@@ -52,7 +53,7 @@ int mysh(void){
 
   //store String in array
   i = 0;
-  argv[i] = strtok ( str_dup, "\n " );
+  argv[i] = strtok ( str, "\n " );
   //check if nothing 
   if(argv[i] == NULL){
       free( argv );
@@ -63,9 +64,6 @@ int mysh(void){
     i++;     
     argv[i] = strtok (NULL, "\n ");
   }
-  argv[i+1] = NULL;//last arg should be NULL as stated in project files
-
-
   
 //******************BUILT IN FUNCTIONALITY************ //
   //check for quit string, exits if user enters "exit" as first argument, other arguments are ignored
@@ -73,11 +71,9 @@ int mysh(void){
   { 
     exit(0);
   }
-  //Code to print out current directory when pwd is inserted
   else if( strcmp( argv[0], pwdString) == 0)
-  { 
-    char currentDir[maxLength]; 
-    if(getcwd(currentDir, sizeof(currentDir))!= NULL){
+  { //Code to print out current directory when pwd is inserted
+   if(getcwd(currentDir, sizeof(currentDir))!= NULL){
       printf("%s\n", currentDir); 
     }
     else
@@ -99,6 +95,7 @@ int mysh(void){
     printf( "%s\n", path);
     if( path == NULL ||  chdir(path ) == -1)
     {
+      printf("free argv\n");
       free( argv );
       return -1;
     }
@@ -113,59 +110,66 @@ int mysh(void){
     if( pid == 0)
     {       
       //begin exec function
-      if( strchr( str, '>') != NULL )
+      i = 0;
+      redirectNotFound = 1;
+      while( i < argc && redirectNotFound )  
       {
-        i = 0;
-        redirectNotFound = 1;
-        while( i <= argc && redirectNotFound )  
+        if( strcmp( argv[i] ,">>\0" ) == 0 || strcmp( argv[i] ,">\0" ) == 0 )
         {
-          if( strcmp( argv[i] ,">>" ) == 0 || strcmp( argv[i] ,">" ) == 0 )
-          {
-            redirectNotFound = 0;//found redirect, assuming only one
+          redirectNotFound = 0;//found redirect, assuming only one
         
-            if( argc != i+1)
-            {
-              return -1;// input after filename, or no filename given
-            }
+          if( argc != i+1+1)//one for zero indexing, one for next element to exist
+          {
+            printf("i+1\n");
+            return -1;// input after filename, or no filename given
+          }
 
-            //change stdout to newfile
-            if(  strcmp( argv[i] ,">>" ) == 0 )
-            {
-              out = open( argv[i], O_APPEND | O_CREAT | O_WRONLY);
-            }
-            if(  strcmp( argv[i] ,">" ) == 0 )
-            {
-              out = open( argv[i], O_TRUNC | O_CREAT | O_WRONLY);
-            }
+          //change stdout to newfile
+          printf( "%s\n", argv[i]);
+          printf( "%s\n", argv[i+1]);
+          printf( "%s\n", argv[i+2]);
+          if(  strcmp( argv[i] ,">>" ) == 0 )
+          {
+            out = open( argv[i+1], O_APPEND | O_CREAT | O_WRONLY);
+          }
+          if(  strcmp( argv[i] ,">" ) == 0 )
+          {
+            out = open( argv[i+1], O_TRUNC | O_CREAT | O_WRONLY);
+          }
 
-            if ( out == -1)
-            {
-              return -1;
-            }
+          if ( out == -1)
+          {
+            printf("out %d\n",out);
+            return -1;
+          }
 
-            if( dup2( 1, out ) == -1)//1 means stdout????
-            {
-              return -1;
-            }
+          if( dup2( out, 1 ) == -1)//1 means stdout????
+          {
+            printf("dup\n");
+            return -1;
+          }
 
-            //create sub array of args
-            if( (sub_argv = malloc( (argc - 2) * sizeof(char*))) != NULL)
-            {
-              return -1;
-            }
-            memcpy( sub_argv, argv, (argc - 2) * sizeof(char*) );
-            free(argv);
-            argv = sub_argv;//to keep naming in rest of the code the same
-          } 
-        }    
+          close(out);
+          //create sub array of args
+          if( (sub_argv = malloc( (argc - 1) * sizeof(char*))) == NULL)//take away null 1 so space for NULL
+          {
+            printf("malloc\n");
+            return -1;
+          }
+          memcpy( sub_argv, argv, (argc - 2) * sizeof(char*) );
+          sub_argv[2] = NULL;
+          free(argv);
+          argv = sub_argv;//to keep naming in rest of the code the same
+        }
+        i++; 
       }
+      printf("%s\n", argv[0]);    
       if( execvp( argv[0], argv) == -1)
       { 
         fprintf(stderr, "Error!\n" );
-        free( argv);
         exit(0);//child process should close even if exec fails
       }
-      exit(0);//end child process
+      exit(0);//Should never reach here
     }
     else
     {
