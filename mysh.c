04877fa30df56841d32d33e2188965cbd2b8b6f8
Benjamin Richardson <brichard@adelie-05.cs.wisc.edu>
2015-02-20 20:02:35 -0600

    Start of redirect

--- mysh.c
+++ mysh.c
@@ -20,7 +20,7 @@ int mysh(void){
   int pid;
   int status;//TODO error checking
   int i; //couter for loops;
-
+  int redirectNotFound;
 
   printf( "mysh>  ");
   if ( fgets( str , maxLength , stdin ) == NULL )
@@ -48,13 +48,12 @@ int mysh(void){
 
   //store String in array
   i = 0;
-  argv[i] = strtok ( str, "\n " );
+  argv[i] = strtok ( str_dup, "\n " );
   //check if nothing 
   if(argv[i] == NULL){
       free( argv );
       return 0;//nothing in command line, restart shell prompt
   }
-
   while( argv[i] != NULL )
   {
     i++;     
@@ -63,6 +62,7 @@ int mysh(void){
   argv[i+1] = NULL;//last arg should be NULL as stated in project files
 
 
+  
 //******************BUILT IN FUNCTIONALITY************ //
   //check for quit string, exits if user enters "exit" as first argument, other arguments are ignored
   if( strcmp( argv[0], quitString) == 0)
@@ -86,21 +86,46 @@ int mysh(void){
     if( argc == 1 )
     {
       path =  getenv("HOME");
-      if( path == NULL ||  chdir(path ) == -1)
-      {
-        return -1;
-      }
     }
     else
     {
       path = argv[1];
-      if( path == NULL ||  chdir(path ) == -1)
-      {
-        return -1;
-      }
-    } 
+    }
+
+    if( path == NULL ||  chdir(path ) == -1)
+    {
+      free( argv );
+      return -1;
+    }
+    //TODO exec events after directory change
   }
- 
+
+  //begin exec function
+  if( strchr( str, '>') != NULL )
+  {
+    i = 0;
+    redirectNotFound = 1;
+    while( i <= argc && redirectNotFound )  
+    {
+      //what about args past the first >?
+      if( strcmp( argv[i] ,">>" ) == 0 || strcmp( argv[i] ,">" ) == 0 )
+      {
+        redirectNotFound = 0;//found redirect, assuming only one
+        if(  strcmp( argv[i] ,">>" ) == 0 )
+        {
+          
+        }
+        if(  strcmp( argv[i] ,">" ) == 0 )
+        {
+          
+        }
+        //check i+1, check i +2
+        //change std out
+        //create sub array of args
+      } 
+    }    
+  }
+
   if( (pid = fork()) == -1)
   {
     return -1;
@@ -111,6 +136,8 @@ int mysh(void){
     {
       if( execvp( argv[0], argv) == -1)
       {
+        //change stdout or std in depending
+        free( argv);
         return -1;
       }
     }
@@ -118,16 +145,14 @@ int mysh(void){
     {
       if( waitpid( pid, &status, 0) == -1)
       {
-       return -1;
-      }
-      else
-      {
-        return 0;
+        free( argv );
+        return -1;
       }
     }
   }
  //free memory of last input
   free( argv);
+  return 0;
 }
 
 int
